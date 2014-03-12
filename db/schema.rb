fc54eab337b83844562988304776e56d7bc9e657
Jiahao Li <isundaylee.reg@gmail.com>
2014-03-12 07:55:46 -0400

    Uses CoverImage class to manage cover images.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140312113933) do
+
+  create_table "cover_images", force: true do |t|
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
